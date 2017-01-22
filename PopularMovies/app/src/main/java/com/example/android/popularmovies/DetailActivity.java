8e7da2f10aa7d363e2b0897025402fb6f34a9dd9
Adyrana <julia.mattjus@gmail.com>
2017-01-22 05:03:26 +0100

    Some small fixes

--- PopularMovies/app/src/main/java/com/example/android/popularmovies/DetailActivity.java
+++ PopularMovies/app/src/main/java/com/example/android/popularmovies/DetailActivity.java
@@ -170,7 +170,24 @@ public class DetailActivity extends AppCompatActivity {
             }
 
             mTitleTextView.setText(movie.getTitle());
+
+            /*
+             * Copyright 2013 Square, Inc.
+             *
+             * Licensed under the Apache License, Version 2.0 (the "License");
+             * you may not use this file except in compliance with the License.
+             * You may obtain a copy of the License at
+             *
+             *    http://www.apache.org/licenses/LICENSE-2.0
+             *
+             * Unless required by applicable law or agreed to in writing, software
+             * distributed under the License is distributed on an "AS IS" BASIS,
+             * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+             * See the License for the specific language governing permissions and
+             * limitations under the License.
+             */
             Picasso.with(mMoviePosterImageView.getContext()).load(posterPath).into(mMoviePosterImageView);
+
             mReleaseDateTextView.setText(releaseDate);
             mRuntimeTextView.setText(movie.getRuntime().toString() + getResources().getString(R.string.detail_minutes));
             mRatingTextView.setText(movie.getVoteAverage().toString() + getResources().getString(R.string.detail_of_ten));
--- PopularMovies/app/src/main/java/com/example/android/popularmovies/TheMovieDbAdapter.java
+++ PopularMovies/app/src/main/java/com/example/android/popularmovies/TheMovieDbAdapter.java
@@ -103,6 +103,22 @@ public class TheMovieDbAdapter extends RecyclerView.Adapter<TheMovieDbAdapter.Th
         String posterPath = NetworkUtils.buildPosterUrl(movie.getPosterPath());
         Log.d(TAG, "movie.getPosterPath(): " + movie.getPosterPath());
         Log.d(TAG, "posterPath: " + posterPath);
+
+        /*
+         * Copyright 2013 Square, Inc.
+         *
+         * Licensed under the Apache License, Version 2.0 (the "License");
+         * you may not use this file except in compliance with the License.
+         * You may obtain a copy of the License at
+         *
+         *    http://www.apache.org/licenses/LICENSE-2.0
+         *
+         * Unless required by applicable law or agreed to in writing, software
+         * distributed under the License is distributed on an "AS IS" BASIS,
+         * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+         * See the License for the specific language governing permissions and
+         * limitations under the License.
+         */
         Picasso.with(theMovieDbAdapterViewHolder.mMoviePosterImageView.getContext()).load(posterPath).into(theMovieDbAdapterViewHolder.mMoviePosterImageView);
     }
 
