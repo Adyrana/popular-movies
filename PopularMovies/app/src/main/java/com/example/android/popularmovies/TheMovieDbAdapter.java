4678fb4c5033b35710fce56ffe7b24957f55aa22
Adyrana <julia.mattjus@gmail.com>
2017-01-22 02:58:22 +0100

    Check if the device is online

--- PopularMovies/app/src/main/java/com/example/android/popularmovies/TheMovieDbAdapter.java
+++ PopularMovies/app/src/main/java/com/example/android/popularmovies/TheMovieDbAdapter.java
@@ -17,8 +17,6 @@
 package com.example.android.popularmovies;
 
 import android.content.Context;
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
--- PopularMovies/app/src/main/java/com/example/android/popularmovies/utilities/ApiKeyUtility.java
+++ PopularMovies/app/src/main/java/com/example/android/popularmovies/utilities/ApiKeyUtility.java
@@ -23,7 +23,6 @@ import android.util.Log;
 import com.example.android.popularmovies.R;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
